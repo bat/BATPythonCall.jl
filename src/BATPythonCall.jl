6793727dc0f81b99d2f80ea19de312a9604bcf8b
Oliver Schulz <oschulz@mpp.mpg.de>
2022-08-10 00:28:48 +0200

    DEBUG

--- src/BATPythonCall.jl
+++ src/BATPythonCall.jl
@@ -1,10 +1,5 @@
 # This file is a part of BATPythonCall.jl, licensed under the MIT License (MIT).
 
-"""
-    BATPythonCall
-
-Template for Julia packages.
-"""
 module BATPythonCall
 
 using PythonCall
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,5 @@ import Test
 
 Test.@testset "Package BATPythonCall" begin
     # include("test_aqua.jl")
-    include("test_pycall_density.jl")
     include("test_docs.jl")
 end # testset
